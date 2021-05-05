81ce095ba8ec5fd7d64fe89baf73d90f43f38401
Surya-mazumder9734 <79822560+Surya-mazumder9734@users.noreply.github.com>
2021-05-05 14:53:46 +0530

    Add files via upload

---
+++ activemq-reciever-service/src/main/java/com/example/demo/ActivemqRecieverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqRecieverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqRecieverServiceApplication.class, args);
+	}
+
+}
